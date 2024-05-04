71fabc2ef494fc6fb904b8f3476b9fa84cb41b25
NALYD <165321899+NALYD2121@users.noreply.github.com>
2024-05-04 12:22:58 +0200

    2

---
+++ op/php/episode.php
@@ -0,0 +1,38 @@
+                               <?php
+                                /*---------------------------------------------------------------*/
+                                /*
+    Titre : Créer un nouveau fichier                                                                                     
+                                                                                                                          
+    URL   : https://phpsources.net/code_s.php?id=337
+    Date édition     : 05 Fév 2008                                                                                        
+    Date mise à jour : 22 Sept 2019                                                                                      
+    Rapport de la maj:                                                                                                    
+    - fonctionnement du code vérifié                                                                                    
+*/
+                                /*---------------------------------------------------------------*/
+
+                                $nom_file = "fichier.php";
+                                $texte = "Hello world!";
+
+                                // création du fichier
+                                $f = fopen($nom_file, "x+");
+                                // écriture
+                                fputs($f, $texte);
+                                // fermeture
+                                fclose($f);
+                                ?>
+
+                               User
+                               <?php
+                                // Boucle pour générer les liens des épisodes
+                                $numEpisodes = 1102; // Nombre total d'épisodes
+                                $baseURL = "https://nalyd2121.github.io/NANIM3/op/";
+
+                                echo '<div class="scroller">';
+                                for ($i = 1; $i <= $numEpisodes; $i++) {
+                                    // Génération du lien avec le numéro d'épisode
+                                    $episodeURL = $baseURL . $i . ".html";
+                                    echo '<a class="dropdowns-item" href="' . $episodeURL . '">Episode ' . $i . '</a>';
+                                }
+                                echo '</div>';
+                                ?>
